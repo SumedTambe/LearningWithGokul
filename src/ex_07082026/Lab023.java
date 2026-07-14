8058405e4ca68971ab23dde193b110f35ccb36d8
SumedTambe <sumed.tambe@gmail.com>
2026-07-14 09:45:59 +0530

    07142026

---
+++ src/ex_07082026/Lab023.java
@@ -0,0 +1,18 @@
+package ex_07082026;
+
+public class Lab023 {
+    public static void main(String[] args) {
+        // Formating //
+        byte b = 120; //%d
+        short s = 32000; //%d
+        int i = 210000000; //%d
+        long l = 210000000000000000L; //%d
+        char c = 'C'; //%c
+        float pi = 3.14f; //%f
+        double d = 3.1029387475849;
+        String str = "Sumed"; //%s
+
+        System.out.printf("This is byte -> %d", b);
+
+    }
+}
